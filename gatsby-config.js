7ff02c467c41e13cee9f6b3d2c3a7462ac02531b
PLANETPINNACLE\juniorm <junior.mader@pinnacle.com>
2021-08-21 16:09:43 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Gatsby by Junior",
+  },
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-sass",
+    "gatsby-plugin-gatsby-cloud",
+  ],
+};
