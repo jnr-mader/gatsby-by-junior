b4e9980ffffe020751d79371d6c3781903087021
PLANETPINNACLE\juniorm <junior.mader@pinnacle.com>
2021-09-21 20:09:47 +0100

    work page populated

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { content } from './style/style.module.scss';
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
-      <div className={content}>
+      <main className={content}>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pellentesque pulvinar tellus, a ultrices ipsum maximus at. Donec non dolor dignissim, finibus massa non, efficitur elit. Fusce elementum tellus nulla, in consectetur quam porta et. Donec bibendum, neque eu cursus tincidunt, nulla leo lacinia tortor, eu blandit eros massa ut orci. Cras maximus augue et ligula egestas, quis auctor libero lobortis. Vestibulum vestibulum at magna a laoreet. Vivamus porta varius mi vel lacinia. In pharetra et libero id luctus.</p>
         <figure>
           <img src="https://picsum.photos/seed/picsum/800/300" alt="Elephant at sunset" />
@@ -14,7 +14,7 @@ const IndexPage = () => {
         <p>Nunc vitae ornare elit, vitae semper est. Aenean semper arcu non nunc imperdiet porta. Pellentesque vitae fermentum purus. Cras condimentum nunc quis odio varius dictum. In lobortis faucibus tellus, sit amet vehicula erat lacinia eu. Suspendisse at nisi urna. Ut scelerisque nunc vitae eros consectetur bibendum. Curabitur dictum sem et mattis accumsan. Fusce sit amet elit at dolor ornare convallis vel in massa. Vivamus non vehicula urna. Donec gravida, mi non egestas pulvinar, massa leo imperdiet eros, sed elementum massa libero ac velit.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pellentesque pulvinar tellus, a ultrices ipsum maximus at. Donec non dolor dignissim, finibus massa non, efficitur elit. Fusce elementum tellus nulla, in consectetur quam porta et. Donec bibendum, neque eu cursus tincidunt, nulla leo lacinia tortor, eu blandit eros massa ut orci. Cras maximus augue et ligula egestas, quis auctor libero lobortis. Vestibulum vestibulum at magna a laoreet. Vivamus porta varius mi vel lacinia. In pharetra et libero id luctus.</p>
         <p>Nunc vitae ornare elit, vitae semper est. Aenean semper arcu non nunc imperdiet porta. Pellentesque vitae fermentum purus. Cras condimentum nunc quis odio varius dictum. In lobortis faucibus tellus, sit amet vehicula erat lacinia eu. Suspendisse at nisi urna. Ut scelerisque nunc vitae eros consectetur bibendum. Curabitur dictum sem et mattis accumsan. Fusce sit amet elit at dolor ornare convallis vel in massa. Vivamus non vehicula urna. Donec gravida, mi non egestas pulvinar, massa leo imperdiet eros, sed elementum massa libero ac velit.</p>
-      </div>
+      </main>
     </Layout>
   )
 }
--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -1,11 +1,64 @@
 import * as React from 'react';
 import Layout from '../../components/layouts/main-layout';
+import { content } from '../style/style.module.scss';
+import { workHeader } from './style.module.scss';
 
 const WorkPage = () => {
   return (
     <Layout pageTitle="My Work">
-        <main>
-            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        <main className={content}>
+          <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+          <section>
+            <h2 className={workHeader}>Front End Developer <span>@ Willowbreeze from Oct 2015 to Now</span></h2>
+            <figure>
+              <img src="https://picsum.photos/seed/picsum/800/300" alt="Elephant at sunset" />
+              <figcaption>Willowbreeze</figcaption>
+            </figure> 
+            <p>
+              Provide support for the marketing and content team of Pinnacle online bookmakers.
+              Site development and maintenance within a .NET environment.
+              Responsive design and development i.e. tablets and mobiles. 
+              Cross-browser compatibility and site optimization, training.  
+            </p> 
+            <p>
+              Code base: HTML5, CSS3, Sass, Javascript, React, Angular, jQuery, Node, Git, Asp.NET
+            </p>
+            <p>
+              Design: Adobe Photoshop, Illustrator, Invision, Zeplin
+            </p>
+          </section> 
+          <section>
+            <h2 className={workHeader}>Junior Developer <span>@ Net Communities from Feb 2010 to Jun 2010</span></h2>
+            <figure>
+              <img src="https://picsum.photos/seed/picsum/800/300" alt="Elephant at sunset" />
+              <figcaption>Net Communities</figcaption>
+            </figure> 
+            <p>Site development and maintenance. Cross-browser compatibility. Html newsletters. Site takeovers for marketing events. Creation of promotional sites.</p> 
+          </section> 
+          <section>
+            <h2 className={workHeader}>Junior Developer <span>@ Net Communities from Feb 2010 to Jun 2010</span></h2>
+            <figure>
+              <img src="https://picsum.photos/seed/picsum/800/300" alt="Elephant at sunset" />
+              <figcaption>Net Communities</figcaption>
+            </figure> 
+            <p>Site development and maintenance. Cross-browser compatibility. Html newsletters. Site takeovers for marketing events. Creation of promotional sites.</p> 
+          </section> 
+          <section>
+            <h2 className={workHeader}>Junior Developer <span>@ Net Communities from Feb 2010 to Jun 2010</span></h2>
+            <figure>
+              <img src="https://picsum.photos/seed/picsum/800/300" alt="Elephant at sunset" />
+              <figcaption>Net Communities</figcaption>
+            </figure> 
+            <p>Site development and maintenance. Cross-browser compatibility. Html newsletters. Site takeovers for marketing events. Creation of promotional sites.</p> 
+          </section> 
+          <section>
+            <h2 className={workHeader}>Junior Developer <span>@ Net Communities from Feb 2010 to Jun 2010</span></h2>
+            <figure>
+              <img src="https://picsum.photos/seed/picsum/800/300" alt="Elephant at sunset" />
+              <figcaption>Net Communities</figcaption>
+            </figure> 
+            <p>Site development and maintenance. Cross-browser compatibility. Html newsletters. Site takeovers for marketing events. Creation of promotional sites.</p> 
+          </section>          
         </main>
     </Layout>    
   )
