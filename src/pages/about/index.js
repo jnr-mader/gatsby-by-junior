d8173b98646a0b492d0fe755a262b154c353ba55
PLANETPINNACLE\juniorm <junior.mader@pinnacle.com>
2021-10-27 17:02:36 +0100

    page height adjusted

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,14 +1,29 @@
 import * as React from 'react';
+import { graphql } from 'gatsby';
 import Layout from '../../components/layouts/main-layout';
+import AboutYaml from '../../../content/about.yml';
 
-const AboutPage = () => {
+const AboutPage = ({data}) => {
   return (
     <Layout pageTitle="About Me">
         <main>
             <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+            <h1>hello :{data.site.siteMetadata.title}</h1>
+            <h1>{AboutYaml.title}</h1>
+            <h1>{AboutYaml.intro}</h1>
         </main>
     </Layout>    
   )
 }
+
+export const query = graphql`
+  query AboutPageQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
 
 export default AboutPage;
