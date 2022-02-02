dd33a895ea8029d3dcae54c563ab5578e2f622e8
PLANETPINNACLE\juniorm <junior.mader@pinnacle.com>
2022-02-02 23:50:13 +0000

    styles for about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../../components/layouts/main-layout';
 import AboutYaml from '../../../content/about.yml';
+import { infoBlocks, infoBlock, imgContainer, innerSkew, textContainer } from './style.module.scss';
 
 const AboutPage = ({data}) => {
   return (
@@ -12,6 +13,23 @@ const AboutPage = ({data}) => {
             <h1>{AboutYaml.title}</h1>
             <h1>{AboutYaml.intro}</h1>
             <div>{AboutYaml.body}</div>
+            <div className={infoBlocks}>
+              <div className={infoBlock}>
+                <div className={imgContainer}>
+                  <div className="img-inner">
+                    <div className={innerSkew}>
+                      <img src="https://picsum.photos/300"/>
+                    </div>
+                  </div>
+                </div>
+                <div class={textContainer}>
+                  <h3>A blue bird</h3>
+                  <div>
+                    This a demo experiment to skew image container. It looks good.
+                  </div>
+                </div>
+              </div>
+            </div>
         </main>
     </Layout>    
   )
