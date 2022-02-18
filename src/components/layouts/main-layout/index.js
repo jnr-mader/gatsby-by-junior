8ba75487c835b8447271e0c6ac514309d5d47fe1
PLANETPINNACLE\juniorm <junior.mader@pinnacle.com>
2022-02-18 00:45:59 +0000

    site title added

--- src/components/layouts/main-layout/index.js
+++ src/components/layouts/main-layout/index.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import classNames from 'classnames';
 
 import "../../../pages/style/global.scss";
-import { wrap, top, innerwrap, mid, end } from './style.module.scss';
+import { wrap, top, innerwrap, mid, end, siteTitle } from './style.module.scss';
 import Navigation from '../../navigation';
 
 const Layout = ({ pageTitle, children }) => {
@@ -12,7 +12,7 @@ const Layout = ({ pageTitle, children }) => {
 
       <div className={classNames(wrap, top)}>
         <div className={innerwrap}>
-          <h2>{pageTitle}</h2>
+        <h2 className={siteTitle}>jnr78</h2><h2>{pageTitle}</h2>
         </div>
       </div>
 
