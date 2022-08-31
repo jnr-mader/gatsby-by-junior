641410c475c2ad7d0667c0c182e6fb8775d89d06
PLANETPINNACLE\juniorm <junior.mader@pinnacle.com>
2022-08-31 22:10:27 +0100

    autocomplete added

---
+++ src/components/search/index.js
@@ -0,0 +1,54 @@
+import * as React from "react";
+import { ReactSearchAutocomplete } from 'react-search-autocomplete';
+
+const Search = ({data}) => {
+    const handleOnSearch = (string, results) => {
+        // onSearch will have as the first callback parameter
+        // the string searched and for the second the results.
+        console.log(string, results)
+    }
+    
+    const handleOnHover = (result) => {
+        // the item hovered
+        console.log(result)
+    }
+    
+    const handleOnSelect = (item) => {
+        // the item selected
+        console.log(item)
+    }
+    
+    const handleOnFocus = () => {
+        console.log('Focused')
+    }
+    
+    // const formatResult = (item) => {
+    //   return (
+    //     <>
+    //       <span style={{ display: 'block', textAlign: 'left' }}>id: {item.id}</span>
+    //       <span style={{ display: 'block', textAlign: 'left' }}>name: {item.name}</span>
+    //     </>
+    //   )
+    // }
+    
+    const fuseOptions = {
+      minMatchCharLength: 2,
+    }
+
+    return(
+        <div>
+            <ReactSearchAutocomplete
+              items={data}
+              onSearch={handleOnSearch}
+              onHover={handleOnHover}
+              onSelect={handleOnSelect}
+              onFocus={handleOnFocus}
+              autoFocus
+            //   formatResult={formatResult}
+              fuseOptions={fuseOptions}
+            />
+        </div>
+    )
+}
+
+export default Search;
--- src/pages/react-demo/index.js
+++ src/pages/react-demo/index.js
@@ -3,12 +3,28 @@ import Layout from '../../components/layouts/demo-layout';
 import ReactDemoYaml from '../../../content/react-demo.yml';
 import { half, content, search, character } from './style.module.scss';
 import { rickAndMortyApi, finalSpaceApi } from '../../helpers/apis';
+import { ReactSearchAutocomplete } from 'react-search-autocomplete'
+import Search from "../../components/search";
 
 const ReactDemo = () => {
+  const [finalSpaceData, setFinalSpaceData] = useState([]);
+  const [rickAndMortyData, setRickAndMortyData] = useState([]);
   const [randomNumberFinalSpace, setRandomNumberFinalSpace] = useState(callRandomCharacter(47));
   const [randomNumberRickAndMorty, setRandomNumberRickAndMorty] = useState(callRandomCharacter(20));
   const [randomStarFinalSpace, setRandomStarFinalSpace] = useState([]);
   const [randomStarRickAndMorty, setRandomStarRickAndMorty] = useState([]);
+
+  useEffect(() => {
+    fetch(`${finalSpaceApi}character/`)
+      .then((res) => res.json())
+      .then((data) => setFinalSpaceData(data));
+  }, []);
+
+  useEffect(() => {
+    fetch(`${rickAndMortyApi}character/`)
+      .then((res) => res.json())
+      .then((data) => setRickAndMortyData(data.results));
+  }, []);
 
   useEffect(() => {
     fetch(`${finalSpaceApi}character/${randomNumberFinalSpace}`)
@@ -39,7 +55,7 @@ const ReactDemo = () => {
       <div className={half}>
         <div className={content}>
           <div className={search}>
-            <input type="text" />
+            <Search data={rickAndMortyData} />
           </div> 
           <div className={character} onClick={(e) => callNewRandomCharacter("rickAndMorty")}>
             <img src={randomStarRickAndMorty.image} />
@@ -51,7 +67,7 @@ const ReactDemo = () => {
       <div className={half}>
         <div className={content}>
           <div className={search}>
-            <input type="text" />
+            <Search data={finalSpaceData} />
           </div>
           <div className={character} onClick={(e) => callNewRandomCharacter("finalSpace")}>
             <img src={randomStarFinalSpace.img_url} />
